5b486376a0be6a2ad229625a1addc13572cd164f
Erickv25 <erickvrp25@gmail.com>
2022-10-09 22:52:55 -0600

    q

--- client/appKoeFood/src/app/core/header/header.component.ts
+++ client/appKoeFood/src/app/core/header/header.component.ts
@@ -9,13 +9,11 @@ import { Router, ActivatedRoute, NavigationEnd, Event } from '@angular/router';
 export class HeaderComponent implements OnInit {
   isSidebarOpen: boolean = false;
   isLoginPage: boolean = true;
-  
+
   constructor(private router: Router) {
-    console.log(router.config[4])
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
-        //console.log(event)
-        this.isLoginPage = event.url == '**' ;
+        this.isLoginPage = event.url == '/';
       }
     });
   }
