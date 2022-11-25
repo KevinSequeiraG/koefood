8eeab11b8ca1071d3bb88137561a21862e144ad4
KevinSequeiraG <kevinsteven.07.sg@gmail.com>
2022-11-24 19:48:26 -0600

    asdf

--- client/appKoeFood/src/app/home/home-routing.module.ts
+++ client/appKoeFood/src/app/home/home-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home/inicio', component: MainPageComponent },
-  { path: 'home/about', component: AboutComponent }
+  { path: 'home/about', component: AboutComponent },
+  { path: 'home/login', component: LoginComponent }
 ];
 
 @NgModule({
